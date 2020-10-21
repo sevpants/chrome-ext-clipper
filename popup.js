8f1fd2a26bafec95524e65e932cbfebadb651f9a
Eric Severn <esevern@sandiego.edu>
2020-10-21 21:45:22 +0000

    added css and js to send/receive clipboard

---
+++ popup.js
@@ -0,0 +1,16 @@
+window.onload  =  function() {
+    chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+    }, function(selection) {
+    chrome.runtime.sendMessage({selection: selection[0]}, function(response) {
+    document.getElementById("output").innerHTML  =  response.clips; 
+    });
+});
+var text = document.getElementById("output")
+
+text.addEventListener("click", function(e) { 
+    chrome.runtime.sendMessage({empty: "clear"}), function(response) {
+    text.innerHTML = response.clips;
+    });
+});
+};
